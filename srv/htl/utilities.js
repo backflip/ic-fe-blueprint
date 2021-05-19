a932340943089ae7d8f73ba18028dbd48797acbc
Thomas Jaggi <thomas@responsive.ch>
2021-05-19 16:32:18 +0200

    feat: work around mock file path conflicts

--- srv/htl/utilities.js
+++ srv/htl/utilities.js
@@ -1,12 +1,10 @@
 module.exports.prepareTemplate = function (template) {
-  const expressions = [
-    /data-sly-use\.templates?=(["'])core.*?\1/,
-    /<sly[^>]+data-sly-call=(["']).*?\1.*?><\/sly>/
-  ]
+  const replaced = template
+    // Remove unsupported parts
+    .replace(/data-sly-use\.templates?=(["'])core.*?\1/g, '')
+    .replace(/<sly[^>]+data-sly-call=(["']).*?\1.*?><\/sly>/g, '')
+    // Replace slashes in resource paths with dashes to prevent conflicts between mock files and directories with the same name
+    .replace(/(data-sly-resource="\${\s+')(.*?)(')/g, (match, p1, p2, p3) => `${p1}${p2.replace(/\//g, '-')}${p3}`)
 
-  expressions.forEach(expression => {
-    template = template.replace(expression, '')
-  })
-
-  return template
+  return replaced
 }
